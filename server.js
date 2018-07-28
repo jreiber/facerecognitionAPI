80a910d15249fe856dfd830a4ccf8d363b26d2b6
Jason Reiber <jason.reiber@sunpowercorp.com>
2018-07-28 13:43:44 -0700

    update port env

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req,res,db, bcrypt
 app.put('/image', (req,res) => { image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) => { image.handleAPICall(req,res)});
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.port || 3000, () => {
+	console.log(`app is running on port ${process.env.port}`);
 });
 
 
