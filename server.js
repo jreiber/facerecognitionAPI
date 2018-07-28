2ff472c85431fea3f27aee92e53557168169b63a
Jason Reiber <jason.reiber@sunpowercorp.com>
2018-07-28 14:17:02 -0700

    capitalize PORT env

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req,res,db, bcrypt
 app.put('/image', (req,res) => { image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) => { image.handleAPICall(req,res)});
 
-app.listen(process.env.port || 3000, () => {
-	console.log(`app is running on port ${process.env.port}`);
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 });
 
 
